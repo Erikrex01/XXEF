0fc6eae86a25e9e54a6deeb144a7939e3257ad77
Erikrex01 <128243342+Erikrex01@users.noreply.github.com>
2023-04-22 22:28:53 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,6 +1,7 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("XXEF", "DarkTheme")
 local Local_Player = Window:NewTab("Local Player")
+wait(0.1)
 local LP_S = Local_Player:NewSection("Section Name")
 LP_S:NewSlider("Walk_Speed", "Walk_Speed", 500, 0, function(s) -- 500 (MaxValue) | 0 (MinValue)
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
