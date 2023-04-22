0cf8daafe1a7df36332a495d390f86c66f17a67d
Erikrex01 <128243342+Erikrex01@users.noreply.github.com>
2023-04-22 22:31:54 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -2,8 +2,12 @@ local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHept
 local Window = Library.CreateLib("XXEF", "DarkTheme")
 local Local_Player = Window:NewTab("Local Player")
 wait(0.1)
-local LP_S = Local_Player:NewSection("Section Name")
-LP_S:NewSlider("Walk_Speed", "Walk_Speed", 500, 0, function(s) -- 500 (MaxValue) | 0 (MinValue)
+local LP_S = Local_Player:NewSection("Player")
+LP_S:NewSlider("Walk_Speed", "Walk Speed", 500, 0, function(s) -- 500 (MaxValue) | 0 (MinValue)
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+    print(s)
+end)
+LP_S:NewSlider("Jump Power", "Jump Power", 500, 0, function(s) -- 500 (MaxValue) | 0 (MinValue)
+    game.Players.LocalPlayer.Character.Humanoid.JumpPower = s
     print(s)
 end)
