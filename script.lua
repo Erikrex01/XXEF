484df80fbfd2c1222faad583ccc25c7e662e64df
Erikrex01 <128243342+Erikrex01@users.noreply.github.com>
2023-04-22 22:25:23 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -4,4 +4,5 @@ local Local_Player = Window:NewTab("Local Player")
 local LC_Section = Local_Player:NewSection("Player")
 LC_Section:NewSlider("Walk_Speed", "Walk_Speed", 500, 0, function(s) -- 500 (MaxValue) | 0 (MinValue)
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+    print(s)
 end)
